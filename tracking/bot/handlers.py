fda0bb4dedcc514630d1bcb03bafb30c1310128b
AlexTolcach <talkatel201444@gmail.com>
2023-06-26 07:52:52 +0300

    Начал проработку меню бота

--- tracking/bot/handlers.py
+++ tracking/bot/handlers.py
@@ -1,19 +1,27 @@
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.dispatcher.filters import Text, Command
-from keyboards import keyboard
 from main import bot, dp
 from config import chat_id
+import keyboards as nav
 
 
 async def send_hello(dp):
-    await bot.send_message(chat_id=chat_id, text='Hello')
+    await bot.send_message(chat_id=chat_id, text='Привет, запишем сегодняшние данные')
 
 
-@dp.message_handler(Command('shop'))
+@dp.message_handler(Command('да'))
 async def shop_shop(message: Message):
-    await message.answer('Shop', reply_markup=keyboard)
+    await message.answer('Начали', reply_markup=nav.other_meny)
 
 
-@dp.message_handler(Text(equals='Внести данные'))
+@dp.message_handler(Text(equals='Расходы'))
 async def get_goods(message: Message):
-    await message.answer(message.text, reply_markup=ReplyKeyboardRemove())
+    await message.answer('Выбери категорию расходов', reply_markup=nav.expenses_meny)
+
+
+@dp.message_handler()
+async def get_other_meny(message: Message):
+    if message.text == 'Главное меню':
+        await message.answer('Выбери какие данные хочешь ввести', reply_markup=nav.other_meny)
+    else:
+        await message.reply('Неизвестная команда')
--- tracking/bot/keyboards.py
+++ tracking/bot/keyboards.py
@@ -2,11 +2,33 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardBut
 from aiogram.utils.callback_data import CallbackData
 
 
-keyboard = ReplyKeyboardMarkup(
+other_meny = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text='Внести данные')
+            KeyboardButton(text='Время работы'),
+            KeyboardButton(text='Расходы'),
+            KeyboardButton(text='Доходы')
         ]
     ],
     resize_keyboard=True
 )
+
+expenses_meny = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Выплаты работникам'),
+            KeyboardButton(text='Покупка и ремонт инструмента'),
+            KeyboardButton(text='Транспортные расходы'),
+        ],
+        [
+            KeyboardButton(text='Главное меню')
+        ]
+    ],
+    resize_keyboard=True
+)
+# btn_main = KeyboardButton(text='Главное меню')
+#
+# btn_time_work = KeyboardButton(text='Время работы')
+# btn_expenses = KeyboardButton(text='Расходы')
+# btn_income = KeyboardButton(text='Доходы')
+
