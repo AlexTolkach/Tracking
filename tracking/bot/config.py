56b6d6caa53e952bb96dcb6439c96a374323acb7
AlexTolcach <talkatel201444@gmail.com>
2023-07-01 13:42:51 +0300

    Разработка меню бота. Установка asyncpg

--- tracking/bot/config.py
+++ tracking/bot/config.py
@@ -5,4 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 chat_id = os.getenv('chat_id')
-print('Bot online')
+
+# bd
+NAME = os.getenv('NAME')
+PG_USER = os.getenv('PG_USER')
+PASSWORD = os.getenv('PASSWORD')
+HOST = os.getenv('HOST')
+PORT = os.getenv('PORT')
+
+POSGRESTURI = f'posgrestsgl://{PG_USER}:{PASSWORD}@{HOST}/{NAME}'
