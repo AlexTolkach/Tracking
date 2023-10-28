2393ca6f903442aa9cd733d936afe4bafb09e35a
AlexTolcach <talkatel201444@gmail.com>
2023-10-28 16:03:19 +0300

    project.html

--- tracking/dataoutput/views.py
+++ tracking/dataoutput/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Project
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, 'home.html')
+
+
+def projects_list(request):
+    context = {
+        "projects": Project.objects.all()
+    }
+    return render(request, "projects.html", context=context)
